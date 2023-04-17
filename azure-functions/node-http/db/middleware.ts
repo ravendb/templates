b877223f3a6c9e8813910b5f4a7f9149616c93cb
Kamran Ayub <kamran.ayub@gmail.com>
2023-04-17 13:43:28 -0500

    feat: node HTTP trigger

---
+++ azure-functions/node-http/db/middleware.ts
@@ -0,0 +1,26 @@
+import { MiddlewareFunction } from "@senacor/azure-function-middleware";
+import { initializeDb, initializeWelcomeDb } from ".";
+import { DocumentConventions, IDocumentStore } from "ravendb";
+
+export const createDbMiddleware =
+  (customize?: (c: DocumentConventions) => void): MiddlewareFunction =>
+  async (ctx, req) => {
+    const isNewSetup = !process.env.DB_URLS || !process.env.DB_NAME;
+    let dbStore: IDocumentStore;
+
+    console.log('URLs', process.env.DB_URLS);
+
+    if (isNewSetup) {
+      dbStore = initializeWelcomeDb();
+    } else {
+      dbStore = await initializeDb({
+        urls: process.env.DB_URLS.split(","),
+        databaseName: process.env.DB_NAME,
+        dbCertPath: process.env.DB_CERT_PATH,
+        dbCertPassword: process.env.DB_CERT_PASSWORD,
+        customize,
+      });
+    }
+
+    ctx.db = dbStore.openSession();
+  };
